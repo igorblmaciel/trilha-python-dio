94fc3f17c59f35332374c49c9478028d7ce18332
igorblmaciel <igorblmaciel@gmail.com>
2024-04-16 22:35:34 -0300

    atualuzaindo 00_full_conjuntos

---
+++ 01 - Estrutura de dados/03 - Conjuntos/00_full_conjuntos.py
@@ -0,0 +1,147 @@
+#Estrutura de dados set (dataset)
+# coleção de objetos unicos
+
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)  # {1, 2, 3, 4}
+
+letras = set("abacaxi")
+print(letras)  # {"b", "a", "c", "x", "i"}
+
+carros = set(("palio", "gol", "celta", "palio"))
+print(carros)  # {"gol", "celta", "palio"}
+
+## acessando dados
+numeros = {1, 2, 3, 2}
+
+numeros = list(numeros)
+
+print(numeros[0])
+
+## iterando Conjuntos
+carros = {"gol", "celta", "palio"}
+
+for carro in carros:
+    print(carro)
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+## uniao de conjuntos
+conjunto_a = {1, 2}
+conjunto_b = {3, 4}
+
+resultado = conjunto_a.union(conjunto_b)
+print(resultado)
+
+## 04 Intersecção
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.intersection(conjunto_b)
+print(resultado)
+
+
+## 05 Diferença
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.difference(conjunto_b)
+print(resultado)
+
+resultado = conjunto_b.difference(conjunto_a)
+print(resultado)
+
+## 06 symetric_diference
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.symmetric_difference(conjunto_b)
+print(resultado)
+
+
+##07 is subset
+conjunto_a = {1, 2, 3}
+conjunto_b = {4, 1, 2, 5, 6, 3}
+
+resultado = conjunto_a.issubset(conjunto_b)  # True
+print(resultado)
+
+resultado = conjunto_b.issubset(conjunto_a)  # False
+print(resultado)
+
+## 08 is dis join
+conjunto_a = {1, 2, 3, 4, 5}
+conjunto_b = {6, 7, 8, 9}
+conjunto_c = {1, 0}
+
+resultado = conjunto_a.isdisjoint(conjunto_b)  # True
+print(resultado)
+
+resultado = conjunto_a.isdisjoint(conjunto_c)  # False
+print(resultado)
+
+##10 add
+sorteio = {1, 23}
+
+sorteio.add(25)  # {1, 23, 25}
+print(sorteio)
+
+sorteio.add(42)  # {1, 23, 25, 42}
+print(sorteio)
+
+sorteio.add(25)  # {1, 23, 25, 42}
+print(sorteio)
+
+#11 Clear
+sorteio = {1, 23}
+
+print(sorteio)  # {1,23}
+
+sorteio.clear()
+
+print(sorteio)  # {}
+
+#12 Copy
+sorteio = {1, 23}
+
+print(sorteio)  # {1, 23}
+
+sorteio.copy()
+
+print(sorteio)  # {1, 23}
+
+#13 Discard
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+numeros.discard(1)
+numeros.discard(45)
+
+print(numeros)  # {2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+#14 Pop
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros.pop())  # 0
+print(numeros.pop())  # 1
+print(numeros)  # {2, 3, 4, 5, 6, 7, 8, 9}
+
+#15 Remove
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros.remove(0))  # 0
+print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+#16 Len
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(len(numeros))  # 10
+
+#17 in
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(1 in numeros)  # True
+print(10 in numeros)  # False
