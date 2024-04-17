3abcdb44a54f47cfbc058593ae596149f368c7ed
igorblmaciel <igorblmaciel@gmail.com>
2024-04-16 21:18:31 -0300

    Atualizando arquivo em Tuplas 00)0Full.py

---
+++ 01 - Estrutura de dados/02 - Tuplas/00_0Full_tuplas.py
@@ -0,0 +1,107 @@
+#### 00 Declarando Tuplas ########
+# Estrutura Imutavel usa tuple ou parentesis
+# boa prática colcoar uma virgula no final para nao confundir com funcoes
+
+
+frutas = (
+    "laranja",
+    "pera",
+    "uva",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
+
+## 01 acesso direto
+frutas = ("maçã", "laranja", "uva", "pera",)
+
+print(frutas[0])  # maçã
+print(frutas[2])  # uva
+
+## 03 Indices Negativos
+frutas = (
+    "maçã",
+    "laranja",
+    "uva",
+    "pera",
+)
+
+print(frutas[-1])  # pera
+print(frutas[-3])  # laranja
+
+
+## 04 Matriz
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
+
+print(matriz[0])  # (1, "a", 2)
+print(matriz[0][0])  # 1
+print(matriz[0][-1])  # 2
+print(matriz[-1][-1])  # "c"
+
+
+## 05 Fatiamentos
+tupla = ("p", "y", "t", "h", "o", "n",)
+
+print(tupla[2:])  # ("t", "h", "o", "n")
+print(tupla[:2])  # ("p", "y")
+print(tupla[1:3])  # ("y", "t")
+print(tupla[0:3:2])  # ("p", "t")
+print(tupla[::])  # ("p", "y", "t", "h", "o", "n")
+print(tupla[::-1])  # ("n", "o", "h", "t", "y", "p")
+
+
+## 06 iterar Tuplas
+carros = (
+    "gol",
+    "celta",
+    "palio",
+)
+
+for carro in carros:
+    print(carro)
+
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+
+## 07 Count
+cores = (
+    "vermelho",
+    "azul",
+    "verde",
+    "azul",
+)
+
+print(cores.count("vermelho"))  # 1
+print(cores.count("azul"))  # 2
+print(cores.count("verde"))  # 1
+
+## 08 index
+linguagens = ("python", "js", "c", "java", "csharp",)
+
+print(linguagens.index("java"))  # 3
+print(linguagens.index("python"))  # 0
+
+## 09 Len
+linguagens = (
+    "python",
+    "js",
+    "c",
+    "java",
+    "csharp",
+)
+
+print(len(linguagens))  # 5
+
